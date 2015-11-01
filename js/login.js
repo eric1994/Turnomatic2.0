64fdb77c82cf0bad998e78afa9e4a072a3a74f2b
MauricioOrta <MauricioOrta@bitbucket.org>
2015-11-01 17:57:11 +0100

    Ampliación de registro

--- js/login.js
+++ js/login.js
@@ -1,44 +1,65 @@
 
 function Registro() {
-    var x3 = document.forms["registerform"]["email"].value;
-	var x4 = document.forms["registerform"]["password"].value;
+    var nombre = document.forms["registerform"]["nombre"].value;
+    var correo = document.forms["registerform"]["email"].value;
+	var contraseña = document.forms["registerform"]["password"].value;
+	var contraseña2 = document.forms["registerform"]["password2"].value;
 	var bandera=0;
 	var re = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-	  if (x3 == null || x3 == "") 
+		 if (nombre == null || nombre == "")
+	 {
+        alert("Debe escribir su nombre");
+		bandera=1;
+		}
+	  if (correo == null || correo == "") 
 	  {
         alert("Debe escribir su email");
 		 bandera=1;
 		}
 		else
 {		
-		if ((re.test(x3))== false )
+		if ((re.test(correo))== false )
 		{
 			alert("Ha escrito su email en un formato incorrecto");
 			 bandera=1;
 			
 		}
 }
-		if (x4 == null || x4  == "")
+		if (contraseña == null || contraseña  == "")
 		{
-		alert("Tiene que escribir una contrase�a");	
+		alert("Tiene que escribir una contraseña");	
 		bandera=1;
+		}
+				if (contraseña != contraseña2 )
+		{
+		alert("Las contraseñas no coinciden");	
+bandera=1;		
 		}
 		if (bandera==1)
 		return false;
 		else
 		{
+		var nombre1 = $('#inputnombre').val();
         var email1 = $('#inputemail').val();
 		var password1 = $('#inputpwd').val();
-				ref.createUser({
+		var sexo1 = $('input[name="genero"]:checked').val();
+		var mes1 = $('#mes').val();
+		var dia1 = $('#dia').val();
+		var anio1 = $('#age').val();
+ref.createUser({
   email    : email1,
   password : password1
 }, function(error, userData) {
   if (error) {
     console.log("Error creating user:", error);
+	if (error.message!="The browser redirected the page before the login request could complete.")
 	alert("Error creando usuario:" + error.message)
   } else {
     console.log("Successfully created user account with uid:", userData.uid);
 	alert("Usuario registrado exitosamente:")
+		ref.push({Nombre: nombre1, Email: email1, Sexo: sexo1, Dia: dia1, Mes: mes1, Año: anio1});
+	window.open("registro.html", "_self")
+	
   }
 })
  return false;  
@@ -49,25 +70,25 @@ function Registro() {
 }
 
 function Login() {
-    var x3 = document.forms["loginform"]["email2"].value;
-	var x4 = document.forms["loginform"]["password2"].value;
+    var correo = document.forms["loginform"]["email2"].value;
+	var contraseña = document.forms["loginform"]["password2"].value;
 	var bandera=0;
 	var re = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-	  if (x3 == null || x3 == "") 
+	  if (correo == null || correo == "") 
 	  {
         alert("Debe escribir su email");
 		 bandera=1;
 		}
 		else
 {		
-		if ((re.test(x3))== false )
+		if ((re.test(correo))== false )
 		{
 			alert("Ha escrito su email en un formato incorrecto");
 			 bandera=1;
 			
 		}
 }
-		if (x4 == null || x4  == "")
+		if (contraseña == null || contraseña  == "")
 		{
 		alert("Tiene que escribir una contrase�a");	
 		bandera=1;
